bb0f3b21c50e24940ff29ce89262bbb2607c0079
Aleksei <abanduko@yahoo.com>
2020-09-23 11:18:43 +0300

    commit_Bandyukov

--- Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android.diceroller
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,17 +10,35 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     // todo (03) Создайте переменную diceImage типа ImageView с отложенной инициализацией
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image);
+        val rollButton : Button = findViewById(R.id.roll_button);
         // todo (05) Создайте и присвойте переменной rollButton значение Button с id = roll_button, используя функцию findViewById
 
         // todo (10) Добавьте clickListener на кнопку rollButton и внутри вызовите функцию rollDice()
+        rollButton.setOnClickListener { rollDice() }
 
         // todo (04) Присвойте переменной diceImage значение ImageView с id = dice_image, используя функцию findViewById
     }
 
+    fun rollDice() {
+
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource);
+    }
     // todo (06) Создайте функцию rollDice. Задания 7-9 выполнить внутри тела функции rollDice()
     // todo (07) Создайте переменную randomInt и присвойте ей значение Random().nextInt(6) + 1
     // todo (08) Создайте переменную drawableResource и присвойте ей значение when (randomInt) {
