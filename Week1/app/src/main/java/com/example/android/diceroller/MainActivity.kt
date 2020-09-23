570b9d9fa37cf714a1d3a2fa5d100e054cf847da
Matvey <70859084+Ferum-bot@users.noreply.github.com>
2020-09-23 18:49:31 +0300

    commit

--- Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -8,16 +8,33 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
     // todo (03) Создайте переменную diceImage типа ImageView с отложенной инициализацией
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // todo (05) Создайте и присвойте переменной rollButton значение Button с id = roll_button, используя функцию findViewById
-
+        val rollButton : Button = findViewById(R.id.roll_button)
         // todo (10) Добавьте clickListener на кнопку rollButton и внутри вызовите функцию rollDice()
-
+        rollButton.setOnClickListener {
+            rollDice()
+        }
         // todo (04) Присвойте переменной diceImage значение ImageView с id = dice_image, используя функцию findViewById
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 
     // todo (06) Создайте функцию rollDice. Задания 7-9 выполнить внутри тела функции rollDice()
