1727de940a6975bf61a431ba1eb2f2a05d3f41d3
Kamillkadze <kamillaa893@gmail.com>
2020-09-24 01:58:02 +0500

    commit

--- Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -7,28 +7,35 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage: ImageView
     // todo (03) Создайте переменную diceImage типа ImageView с отложенной инициализацией
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // todo (05) Создайте и присвойте переменной rollButton значение Button с id = roll_button, используя функцию findViewById
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            rollDice()
+        }
 
         // todo (10) Добавьте clickListener на кнопку rollButton и внутри вызовите функцию rollDice()
-
+        diceImage = findViewById(R.id.dice_image)
         // todo (04) Присвойте переменной diceImage значение ImageView с id = dice_image, используя функцию findViewById
     }
 
-    // todo (06) Создайте функцию rollDice. Задания 7-9 выполнить внутри тела функции rollDice()
-    // todo (07) Создайте переменную randomInt и присвойте ей значение Random().nextInt(6) + 1
-    // todo (08) Создайте переменную drawableResource и присвойте ей значение when (randomInt) {
-    //            1 -> R.drawable.dice_1
-    //            2 -> R.drawable.dice_2
-    //            3 -> R.drawable.dice_3
-    //            4 -> R.drawable.dice_4
-    //            5 -> R.drawable.dice_5
-    //            else -> R.drawable.dice_6
-    //        }
-    // todo (09) Поставьте в diceImage полученный drawableResource с помощью функции setImageResource
+    fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+        diceImage.setImageResource(drawableResource)
+    }
 }
+
