2e07952685227386e33a0993878ba4daf2d696e0
wnikiti4 <wnikiti4@gmail.com>
2020-09-24 11:47:26 +0300

    Commit Done todo(1-10)

--- Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Week1/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,34 +1,46 @@
 package com.example.android.diceroller
 
+import android.annotation.SuppressLint
+import android.icu.number.IntegerWidth
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    // todo (03) Создайте переменную diceImage типа ImageView с отложенной инициализацией
+    //Done todo (03) Создайте переменную diceImage типа ImageView с отложенной инициализацией
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // todo (05) Создайте и присвойте переменной rollButton значение Button с id = roll_button, используя функцию findViewById
-
-        // todo (10) Добавьте clickListener на кнопку rollButton и внутри вызовите функцию rollDice()
-
-        // todo (04) Присвойте переменной diceImage значение ImageView с id = dice_image, используя функцию findViewById
+        // Done todo (05) Создайте и присвойте переменной rollButton значение Button с id = roll_button, используя функцию findViewById
+        val rollButton : Button = findViewById(R.id.roll_button);
+        // Done todo (10) Добавьте clickListener на кнопку rollButton и внутри вызовите функцию rollDice()
+        rollButton.setOnClickListener{rollDice();}
+        // Done todo (04) Присвойте переменной diceImage значение ImageView с id = dice_image, используя функцию findViewById
+        diceImage = findViewById(R.id.dice_image);
     }
 
-    // todo (06) Создайте функцию rollDice. Задания 7-9 выполнить внутри тела функции rollDice()
-    // todo (07) Создайте переменную randomInt и присвойте ей значение Random().nextInt(6) + 1
-    // todo (08) Создайте переменную drawableResource и присвойте ей значение when (randomInt) {
-    //            1 -> R.drawable.dice_1
-    //            2 -> R.drawable.dice_2
-    //            3 -> R.drawable.dice_3
-    //            4 -> R.drawable.dice_4
-    //            5 -> R.drawable.dice_5
-    //            else -> R.drawable.dice_6
-    //        }
-    // todo (09) Поставьте в diceImage полученный drawableResource с помощью функции setImageResource
-}
+    //Done todo (06) Создайте функцию rollDice. Задания 7-9 выполнить внутри тела функции rollDice()
+    @Deprecated("Я не смог нормально подключить пространстро имен random, так что сделал todo (07) коряво")
+    fun rollDice(){
+        //Warning  todo (07) Создайте переменную randomInt и присвойте ей значение Random().nextInt(6) + 1
+        val randomInt : Int = kotlin.random.Random.nextInt(1 ) + 1;
+            //Done todo (08) Создайте переменную drawableResource и присвойте ей значение when (randomInt) {
+            val drawableResource = when(randomInt){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            };
+        //Done todo (09) Поставьте в diceImage полученный drawableResource с помощью функции setImageResource
+        diceImage.setImageResource(drawableResource);
+        }
+    }
