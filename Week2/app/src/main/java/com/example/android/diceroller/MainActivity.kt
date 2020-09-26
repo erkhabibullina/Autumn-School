b8ce6ec7a5564fcfa89a8c0c0db0bc5672c3b4b0
sshlapakovvlad <60739826+sshlapakovvlad@users.noreply.github.com>
2020-09-26 13:43:24 +0300

    Revert "Merge remote-tracking branch 'origin/master' into w1/hw_alex_anisimov"
    This reverts commit 5a3ab56d309e8a5e9b7679ea59542aa958a76ca0, reversing
    changes made to c645a2ceaf5e768ebf512ec2ad347afd1f76f0c1.

--- Week2/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Week2/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -6,33 +6,33 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
-// todo (08) Создайте класс MainActivity, который будет расширять класс AppCompatActivity()
-lateinit var diceImage: ImageView
+class MainActivity : AppCompatActivity() {
 
-// todo (09) добавьте модификатор переписывания к функции onCreate()
-fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    lateinit var diceImage: ImageView
 
-    val rollButton: Button = findViewById(R.id.roll_button)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    rollButton.setOnClickListener {
-        rollDice()
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
-    diceImage = findViewById(R.id.dice_image)
-}
-
-
-fun rollDice() {
-    val randomInt = Random.nextInt(6) + 1
-    val drawableResource = when (randomInt) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
-    diceImage.setImageResource(drawableResource)
 }
