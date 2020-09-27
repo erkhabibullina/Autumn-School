f0f5d3237d37d10285b6d669fda57384e5099302
EugeneMoon22 <moontyan223@yandex.ru>
2020-09-27 22:52:07 +0300

    commit

--- Week2/app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ Week2/app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -17,22 +17,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
 
         rollButton.setOnClickListener {
-            rollDice()
+            rollDice(diceImage)
         }
-
         diceImage = findViewById(R.id.dice_image)
     }
 
-    private fun rollDice() {
-        val randomInt = Random.nextInt(6) + 1
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableResource)
-    }
 }
