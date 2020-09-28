0cb9b77c45d19e8303c609d845f11d4e0cd3b220
erkhabibullina <erkhabibullina@gmail.com>
2020-09-28 11:33:24 +0300

    clear files and README.md

--- Week3/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ Week3/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.colormyviews
 
 import android.graphics.Color
@@ -50,7 +34,7 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored() {
         // todo добавьте блок when, в котором мы поставим бекграунды для View в зависимости от её id
 
-        // todo поставьте box_one_text и box_two_text бекграунды соответственно тёмно-серого и серого цвета, используя класс Color
+            // todo поставьте box_one_text и box_two_text бекграунды соответственно тёмно-серого и серого цвета, используя класс Color
 
         // todo поставьте box_three_text, box_four_text и box_five_text бекграунды соответственно holo_green_light, holo_green_dark и holo_green_light, используя Android color resources
 
