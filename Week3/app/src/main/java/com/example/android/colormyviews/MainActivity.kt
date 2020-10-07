3b88e9cd9611510383abc85724163613be036834
Matvey <70859084+Ferum-bot@users.noreply.github.com>
2020-10-07 21:06:01 +0300

    commit

--- Week3/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ Week3/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
