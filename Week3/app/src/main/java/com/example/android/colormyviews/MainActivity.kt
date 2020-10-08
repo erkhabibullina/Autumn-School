749f1170f86bbd9d774cabf407d324dd908d2bd8
AivazovaKristina <ksayvazova@edu.hse.ru>
2020-10-08 14:25:23 +0300

    commit

--- Week3/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ Week3/app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.android.colormyviews
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    // todo (08) наследуйте и перепишите функцию onCreate
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
     // todo (09) соедините layout (activity_main.xml) с классом activity (MainActivity)
     // todo (10) добавьте listeners для всех View вашего лэйаута
 
@@ -17,8 +22,13 @@ class MainActivity : AppCompatActivity() {
      */
     private fun setListeners() {
         // todo (11) создайте переменную, в которой будет храниться список всех View из @layout/activity_main (их должно быть 9)
-
+        val clickableViews : List<View> = listOf(box_one_text,box_two_text,box_three_text,
+                box_four_text,box_five_text,constraint_layout,
+                red_button,green_button,yellow_button)
         // todo (12) используя цикл, для всех View в clickableViews назначьте listener, такой, чтобы View меняла цвет при нажатии на неё
+        for (item in clickableViews){
+            item.setOnClickListener{makeColored(it)}
+        }
     }
 
     /**
@@ -31,7 +41,20 @@ class MainActivity : AppCompatActivity() {
      *     * Using a custom color defined in color.xml
      */
     // todo (13) добавьте параметр view типа View для makeColored()
-    private fun makeColored() {
+    private fun makeColored(view: View) {
+        when(view.id){
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
         // todo (14) добавьте блок when, в котором мы поставим бекграунды для View в зависимости от её id
 
             // todo (15) поставьте box_one_text и box_two_text бекграунды соответственно тёмно-серого и серого цвета, используя класс Color
