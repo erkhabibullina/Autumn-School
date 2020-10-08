40fc38a4e782b53c40290b7ab1427b6538d83683
sshlapakovvlad <60739826+sshlapakovvlad@users.noreply.github.com>
2020-10-08 12:47:45 +0300

    week 4

---
+++ Week4/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -0,0 +1,144 @@
+
+package com.example.android.dessertpusher
+
+import android.content.ActivityNotFoundException
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleObserver
+import com.example.android.dessertpusher.databinding.ActivityMainBinding
+import timber.log.Timber
+
+/** onSaveInstanceState Bundle Keys **/
+// todo (11) создать ключи доступа для бандлов данных
+
+class MainActivity : AppCompatActivity(), LifecycleObserver {
+
+    private var revenue = 0
+    private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
+
+    // Contains all the views
+    private lateinit var binding: ActivityMainBinding
+
+    /** Dessert Data **/
+
+    /**
+     * Simple data class that represents a dessert. Includes the resource id integer associated with
+     * the image, the price it's sold for, and the startProductionAmount, which determines when
+     * the dessert starts to be produced.
+     */
+    data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
+
+    // Create a list of all desserts, in order of when they start being produced
+    private val allDesserts = listOf(
+            Dessert(R.drawable.cupcake, 5, 0),
+            Dessert(R.drawable.donut, 10, 5),
+            Dessert(R.drawable.eclair, 15, 20),
+            Dessert(R.drawable.froyo, 30, 50),
+            Dessert(R.drawable.gingerbread, 50, 100),
+            Dessert(R.drawable.honeycomb, 100, 200),
+            Dessert(R.drawable.icecreamsandwich, 500, 500),
+            Dessert(R.drawable.jellybean, 1000, 1000),
+            Dessert(R.drawable.kitkat, 2000, 2000),
+            Dessert(R.drawable.lollipop, 3000, 4000),
+            Dessert(R.drawable.marshmallow, 4000, 8000),
+            Dessert(R.drawable.nougat, 5000, 16000),
+            Dessert(R.drawable.oreo, 6000, 20000)
+    )
+    private var currentDessert = allDesserts[0]
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.i("onCreate Called")
+
+        // Use Data Binding to get reference to the views
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
+        // todo (09) присвоить dessertTimer инстанс DessertTimer, указав в параметрах лайфсайкл
+
+
+        // todo (12) Проверить если есть savedInstanceState, тогда забрать оттуда данные
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    /**
+     * Updates the score when the dessert is clicked. Possibly shows a new dessert.
+     */
+    private fun onDessertClicked() {
+
+        revenue += currentDessert.price
+        dessertsSold++
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        showCurrentDessert()
+    }
+
+    /**
+     * Determine which dessert to show.
+     */
+    private fun showCurrentDessert() {
+        var newDessert = allDesserts[0]
+        for (dessert in allDesserts) {
+            if (dessertsSold >= dessert.startProductionAmount) {
+                newDessert = dessert
+            } else break
+        }
+
+        if (newDessert != currentDessert) {
+            currentDessert = newDessert
+            binding.dessertButton.setImageResource(newDessert.imageId)
+        }
+    }
+
+    /**
+     * Menu methods
+     */
+    private fun onShare() {
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setText(getString(R.string.share_text, dessertsSold, revenue))
+                .setType("text/plain")
+                .intent
+        try {
+            startActivity(shareIntent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(this, getString(R.string.sharing_not_available),
+                    Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.shareMenuButton -> onShare()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Called when the user navigates away from the app but might come back
+     */
+    // todo (10) оверрайднуть onSaveInstanceState для того, чтобы сохранять данные в кэш
+    // todo  если приложение стопается, и onRestoreInstanceState, чтобы получать эти данные
+
+    /** Lifecycle Methods **/
+    // todo (04) написать все лайфсайкл колбеки
+}
