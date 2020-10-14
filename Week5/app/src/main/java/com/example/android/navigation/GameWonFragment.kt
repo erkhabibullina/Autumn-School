896a1548883258c6302cd0873e69b1e961750ff5
sshlapakovvlad <60739826+sshlapakovvlad@users.noreply.github.com>
2020-10-14 12:15:26 +0300

    commit 5 week

---
+++ Week5/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import androidx.databinding.DataBindingUtil
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.core.app.ShareCompat
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentGameWonBinding
+import android.content.pm.ResolveInfo
+import android.content.pm.PackageManager
+
+
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_game_won, container, false)
+        // todo сделать так, чтобы при нажатии на nextMatchButton мы бы перешли на GameFragment
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                    GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+        }
+        // todo поставить true у setHasOptionsMenu
+        return binding.root
+    }
+
+    // todo написать метод getShareIntent, который вернет share intent с
+    // todo текстом R.string.share_success_text и аргументами numCorrect и NumQuestions
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        return ShareCompat.IntentBuilder.from(activity!!)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
+    }
+
+    // todo написать метод shareSuccess(), который запустит активити с аргументом getShareIntent
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    // todo переписать onCreateOptionsMenu, в котором мы будем инфлейтить меню и проверять, находимся
+    // todo мы на экране активити или нет, и если нет, то прятать значки меню
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        // check if the activity resolves
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+            // hide the menu item if it doesn't resolve
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    // todo переписать onOptionsItemSelected, который будет проверять все item.itemId, и если он равен R.id.share,
+    // todo то вызывать метод shareSuccess()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ Week5/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import androidx.databinding.DataBindingUtil
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    // todo сделать отложенную переменную drawerLayout, которая экстендид DrawerLayout
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration : AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        // todo присвоить переменной drawerLayout - binding.drawerLayout
+        drawerLayout = binding.drawerLayout
+        // todo сделать переменную navController, которая будет равна this.findNavController(R.id.myNavHostFragment)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        // todo с помощью NavigationUI засетапить экшн бар
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        // todo инициализировать appBarConfiguration с помощью AppBarConfiguration
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        // prevent nav gesture if not on start destination
+        // todo предотвратить возможность свайпа навдровера не со старт дестинейшна
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, bundle: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+        // todo с помощью NavigationUI засетапить нав контроллер
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    // todo переписать onSupportNavigateUp, где инициализировать заново навконтроллер и ретернуть
+    // todo NavigationUI.navigateUp
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+}
