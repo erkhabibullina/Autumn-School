afd2b8f4ce02ffe351baef0d3c78fc02df6c2016
GhostFantik <ghostfantik@gmail.com>
2020-10-14 21:37:37 +0300

    Home Work W4

--- Week4/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
+++ Week4/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
@@ -24,7 +24,7 @@ import timber.log.Timber
  */
 
 // todo (05) унаследовать LifecycleObserver, передать в параметры Lifecycle
-class DessertTimer {
+class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
 
     var secondsCount = 0
 
@@ -37,8 +37,12 @@ class DessertTimer {
 
 
     // todo (06) добавить обсервер к нашему Lifecycle
+    init {
+        lifecycle.addObserver(this)
+    }
 
     // todo (07) используя lifecycle library сделать так, чтобы startTime запускался на OnStart
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startTimer() {
         // Create the runnable action, which prints out a log and increments the seconds counter
         runnable = Runnable {
@@ -57,6 +61,7 @@ class DessertTimer {
     }
 
     // todo (08) используя lifecycle library сделать так, чтобы startTime запускался на OnStop
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stopTimer() {
         // Removes all pending posts of runnable from the handler's queue, effectively stopping the
         // timer
